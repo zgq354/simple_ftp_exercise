d65e5815d30c637134bd1a0d48029d0e6b6b3b12
qing <zgq354@foxmail.com>
2018-08-24 00:10:16 +0800

    CWD & LIST

--- server.py
+++ server.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
 import socket
 import threading
+import os
 
 # temporary user list
 users = [
   {
     'name': 'admin',
     'passwd': 'password',
-    'path': '/home'
+    'path': '/'
   },
   {
     'name': 'user2',
     'passwd': 'password',
     'path': '/home'
+  },
+  {
+    'name': 'anonymous',
+    'passwd': 'anon@localhost',
+    'path': '/'
   }
 ]
 
@@ -27,7 +33,7 @@ class Counter():
       self.connect_count += 1
     finally:
       self.counter_lock.release()
-    print("%d connections" % self.connect_count)
+    print("count: %d connections" % self.connect_count)
 
   def decrease(self):
     self.counter_lock.acquire()
@@ -35,33 +41,117 @@ class Counter():
       self.connect_count -= 1
     finally:
       self.counter_lock.release()
-    print("%d connections" % self.connect_count)
+    print("count: %d connections" % self.connect_count)
 
 counter = Counter()
 
+class Path():
+  def __init__(self, base):
+    self.base = base
+    self.wd = '/'
+
+  def cwd(self, new_wd):
+    if new_wd[:1] == '/':
+      self.wd = new_wd
+    elif new_wd[:2] == './':
+      self.wd += new_wd[2:]
+    elif new_wd[:2] == '..':
+      if self.wd[-1:] == '/':
+        self.wd = self.wd[:-1]
+      self.wd = '/'.join(self.wd.split('/')[:-1])
+      new_wd = new_wd[2:]
+      if new_wd[:1] != '/':
+        new_wd = '/' + new_wd
+      self.wd += new_wd
+    else:
+      if (self.wd[-1] == '/' and new_wd[:1] == '/'):
+        self.wd += new_wd[1:]
+      elif (self.wd[-1] != '/' and new_wd[:1] != '/'):
+        self.wd += '/' + new_wd
+      else:
+        self.wd += new_wd
+    print('wd:', self.wd)
+  
+  def get(self):
+    return self.wd
+
+  def getAbs(self):
+    return self.base + self.wd
+
+
 class Conn(threading.Thread):
-  def __init__(self, fd):
+  def __init__(self, fd, addr):
     threading.Thread.__init__(self)
     self.fd = fd
+    self.addr = addr
+    self.data_fd = False
     self.running = True
     self.user = False
     self.username = False
     self.passwd = False
+    self.wd = Path(os.getcwd())
 
-  def pocess_command(self, command, args):
-    command = command.upper()
-    if command == 'USER':
+  def pocess_command(self, cmd, args):
+    cmd = cmd.upper()
+    # print(cmd, args)
+    if cmd == 'USER':
       self.username = args
       if self.user:
         self.user = False
         self.message(331, 'Previous account information was flushed, send password.')
       else:
         self.message(331, 'Username ok, send password.')
-    elif command == 'PASS':
+    elif cmd == 'PASS':
       self.passwd = args
       self.login()
+    elif cmd == "TYPE":
+      self.message(200, "ok")  
+    elif cmd == 'PWD':
+      if self.need_login():
+        self.message(257, "\"%s\" is the current directory." % self.wd.wd)
+    elif cmd == 'CWD':
+      if self.need_login():
+        self.wd.cwd(args)
+        self.message(250, "\"%s\" is the current directory." % self.wd.wd)
+    elif cmd == 'PASV':
+      if self.need_login():
+        self.init_datafd()
+      pass
+    elif cmd == 'LIST':
+      if self.need_login():
+        self.send_list()
+      pass
     else:
       self.message(500, 'Syntax error, command unrecognized.')
+  
+  def send_list(self):
+    s, addr = self.data_fd.accept()
+    print('data: send to ', addr)
+    result = os.popen('ls -al %s' % self.wd.getAbs()).read()
+    # LF to CRLF
+    lines = result.split('\n')
+    self.message(150, 'start sending data')
+    for line in lines:
+      s.send((line + '\r\n').encode('utf-8'))
+    self.data_fd.close()
+    self.message(226, 'Transfer complete')
+
+  def init_datafd(self):
+    try:
+      self.data_fd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      self.data_fd.bind(('0.0.0.0', 0))
+      self.data_fd.listen()
+      ip, port = self.data_fd.getsockname()
+      addr = ','.join(ip.split('.')) + ',' + str(port >> 8) + ',' + str(port & 255)
+      self.message(227, "Entering Passive Mode (%s)." % addr)
+    except:
+      self.message(500, 'Failed create data socket')
+
+  def need_login(self):
+    if not self.user:
+      self.message(530, 'Log in with USER and PASS first.')
+      return False
+    return True
 
   def login(self):
     if not self.username:
@@ -79,11 +169,13 @@ class Conn(threading.Thread):
       else:
         # success
         self.user = user
-        print(self.user)
+        self.wd.cwd(user['path'])
+        print('login:', self.user)
         self.message(230, 'Login successful.')
         pass
 
   def message(self, code, data):
+    print('reply:', code, data)
     self.fd.send(("%d %s\r\n" % (code, data)).encode('utf-8'))
 
   def run(self):
@@ -92,7 +184,6 @@ class Conn(threading.Thread):
     raw = ''
     while self.running:
       recv = self.fd.recv(2048)
-      print(recv)
       if not recv:
         break
       if len(recv) == 0 :
@@ -100,24 +191,26 @@ class Conn(threading.Thread):
       raw += recv.decode('utf-8')
       if raw[-2:] != '\r\n':
         continue
+      print('client:', raw[:-2])
+      raw = raw[:-2]
       command = raw.split(' ')[0]
-      args = raw[(len(command) + 1):-2]
+      args = raw[(len(command) + 1):]
       raw = ''
       # process ftp command
       self.pocess_command(command, args)
-    print('Close connection')
+    print('conn: Close connection')
     counter.decrease()
     self.running = False
     self.fd.close()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('0.0.0.0', 2233))
+s.bind(('0.0.0.0', 22333))
 s.listen()
 
 print('Waiting for connect...')
 while True:
   # 接受连接
   sock, addr = s.accept()
-  t = Conn(sock)
+  t = Conn(sock, addr)
   counter.increase()
   t.start()
